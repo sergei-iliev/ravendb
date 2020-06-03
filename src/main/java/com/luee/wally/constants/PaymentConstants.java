d949f418bc1a07048f27714a66b087dd6c46fabd
sergei_iliev <sergei.iliev@gmail.com>
2020-06-03 16:01:59 +0300

    bug

--- src/main/java/com/luee/wally/constants/PaymentConstants.java
+++ src/main/java/com/luee/wally/constants/PaymentConstants.java
@@ -8,6 +8,8 @@ public interface PaymentConstants {
 	public static final BigDecimal  SINGLE_PAYMENT=new BigDecimal(30.0);	
 	public static final BigDecimal  TOTAL_PAYMENTS_PER_USER=new BigDecimal(100.0);
 	public static final BigDecimal  TOTAL_DAILY_PAYMENT=new BigDecimal(1000.0);
+	
+	public static final Double  BLOCK_PAYMENT_VALUE=new Double(5.0);
 
 	
 }
